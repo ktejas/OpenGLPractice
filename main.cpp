684c486d9fa66dcb90bcca395d6b48bbd483231a
ktejas <krishnasalimeda@gmail.com>
2017-01-05 20:04:12 -0700

    Added temporary directory to store all temp files.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,16 @@
 //Global variables
 GLuint VBO;
 
+void CreateVertexBuffer()
+{
+	Vector3f vertices[1];
+	vertices[0] = Vector3f(0.0f, 0.0f, 0.0f);
+
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+}
+
 void RenderSceneCB()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
@@ -13,9 +23,7 @@ void RenderSceneCB()
 	glEnableVertexAttribArray(0);
 	glGenBuffers(1, &VBO);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-
 	glDrawArrays(GL_POINTS, 0, 1);
-
 	glDisableVertexAttribArray(0);
 
 	glutSwapBuffers();
@@ -26,39 +34,31 @@ void InitializeGlutCallbacks()
 	glutDisplayFunc(RenderSceneCB);
 }
 
-void CreateVertexBuffer()
-{
-	Vector3f vertices[1];
-	vertices[0] = Vector3f(0.0f, 0.0f, 0.0f);
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-}
-
 int main(int argc, char ** argv)
 {
-	//Initialize GLUT and GLEW
+	//Initialization
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowSize(800, 600);
 	glutInitWindowPosition(100, 100);
+
 	glutCreateWindow("OGL Practice 02");
 
-	InitializeGlutCallbacks();
-
-	GLenum result = glewInit();
-	if (result != GLEW_OK)
+	GLenum glewResult = glewInit();
+	if (glewResult != GLEW_OK)
 	{
-		fprintf(stderr, "Error: %s.\n", glewGetErrorString(result));
+		fprintf(stderr, "Error: %s.\n", glewGetErrorString(glewResult));
 		return 1;
 	}
 
-	//Draw stuff
-	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-
+	InitializeGlutCallbacks();
+	
+	//Draw
 	CreateVertexBuffer();
 
-	//Update stuff
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+	//Update
 	glutMainLoop();
 
 	return 0;
