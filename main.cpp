b93f6c23fce37805a1724a70c3c9d550a5e41373
ktejas <krishnasalimeda@gmail.com>
2017-01-09 20:49:34 -0700

    Added Rotation to vertex shader.

--- main.cpp
+++ main.cpp
@@ -4,17 +4,18 @@
 
 #include "OGL\Include\glew.h"
 #include "OGL\Include\freeglut.h"
-#include "OGL\Include\ogldev_math_3d.h"
 #include "OGL\Include\ogldev_util.h"
+#include "OGL\Include\ogldev_math_3d.h"
 
 //Variables
 GLuint VBO;
+const GLchar * pVSFileName = "Assets/Shaders/vertex.shader";
+const GLchar * pFSFileName = "Assets/Shaders/fragment.shader";
 GLuint gScaleLocation;
 GLuint gWorldLocation;
-const char * pVSFileName = "Assets/Shaders/vertex.shader";
-const char * pFSFileName = "Assets/Shaders/fragment.shader";
 
 //Functions
+
 static void AddShader(GLuint ShaderProgram, const char * pShaderSource, GLenum ShaderType)
 {
 	GLuint ShaderObj = glCreateShader(ShaderType);
@@ -31,14 +32,14 @@ static void AddShader(GLuint ShaderProgram, const char * pShaderSource, GLenum S
 	glShaderSource(ShaderObj, 1, p, Lengths);
 
 	glCompileShader(ShaderObj);
-	
+
 	GLint Success = 0;
 	GLchar InfoLog[1024] = { 0 };
 	glGetShaderiv(ShaderObj, GL_COMPILE_STATUS, &Success);
 	if (!Success)
 	{
 		glGetShaderInfoLog(ShaderObj, sizeof(InfoLog), NULL, InfoLog);
-		fprintf(stderr, "Error compiling shader: %s of type %d.\n", InfoLog, ShaderType);
+		fprintf(stderr, "Error compiling shader: %s of type: %d.\n", InfoLog, ShaderType);
 		exit(1);
 	}
 
@@ -52,19 +53,19 @@ void CompileShaders()
 	GLuint ShaderProgram = glCreateProgram();
 	if (!ShaderProgram)
 	{
-		fprintf(stderr, "Error creating Shader Program.\n");
+		fprintf(stderr, "Error creatinf Shdaer Program.\n");
 		exit(1);
 	}
 
 	if (!ReadFile(pVSFileName, vs))
 	{
-		fprintf(stderr, "Error reading Vertex Shader.");
+		fprintf(stderr, "Error reading vertex shader.\n");
 		exit(1);
 	}
 
 	if (!ReadFile(pFSFileName, fs))
 	{
-		fprintf(stderr, "Error reading Fragment Shader.");
+		fprintf(stderr, "Error reading fragment shader.\n");
 		exit(1);
 	}
 
@@ -79,7 +80,7 @@ void CompileShaders()
 	if (!Success)
 	{
 		glGetProgramInfoLog(ShaderProgram, sizeof(InfoLog), NULL, InfoLog);
-		fprintf(stderr, "Error linking Shader Program: %s.\n", InfoLog);
+		fprintf(stderr, "Error linking shader program: %s.\n", InfoLog);
 		exit(1);
 	}
 
@@ -95,7 +96,7 @@ void CompileShaders()
 
 	glUseProgram(ShaderProgram);
 
-	//Getting the Uniform Variables
+	//Getting uniform variables
 	gScaleLocation = glGetUniformLocation(ShaderProgram, "gScale");
 	assert(gScaleLocation != 0xFFFFFFFF);
 
@@ -119,21 +120,20 @@ void RenderSceneCB()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	//Update the scale
+	//Scaling
 	static float scale = 0.0f;
 	scale += 0.02f;
 	glUniform1f(gScaleLocation, sinf(scale));
 
-	//Translation Matrix
+	//Translation and Rotation
 	Matrix4f world;
-	world.m[0][0] = 1.0f; world.m[0][1] = 0.0f; world.m[0][2] = 0.0f; world.m[0][3] = cosf(scale);
-	world.m[1][0] = 0.0f; world.m[1][1] = 1.0f; world.m[1][2] = 0.0f; world.m[1][3] = 0.0f;
+	world.m[0][0] = cosf(scale); world.m[0][1] = -sinf(scale); world.m[0][2] = 0.0f; world.m[0][3] = sinf(scale);
+	world.m[1][0] = sinf(scale); world.m[1][1] = cosf(scale); world.m[1][2] = 0.0f; world.m[1][3] = 0.0f;
 	world.m[2][0] = 0.0f; world.m[2][1] = 0.0f; world.m[2][2] = 1.0f; world.m[2][3] = 0.0f;
 	world.m[3][0] = 0.0f; world.m[3][1] = 0.0f; world.m[3][2] = 0.0f; world.m[3][3] = 1.0f;
-
 	glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, &world.m[0][0]);
 
-	//Draw a Triangle
+	//Draw Triangle
 	glEnableVertexAttribArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
@@ -157,12 +157,12 @@ int main(int argc, char ** argv)
 	glutInitWindowSize(512, 512);
 	glutInitWindowPosition(100, 100);
 
-	glutCreateWindow("OGL Practice 05");
+	glutCreateWindow("OGL PRactice 06");
 
 	GLenum glewResult = glewInit();
 	if (glewResult != GLEW_OK)
 	{
-		fprintf(stderr, "Error initializing GLEW: %s.\n", glewGetErrorString(glewResult));
+		fprintf(stderr, "Error initialization GLEW: %s.\n", glewGetErrorString(glewResult));
 		exit(1);
 	}
 
@@ -179,6 +179,6 @@ int main(int argc, char ** argv)
 
 	//Update
 	glutMainLoop();
-
+	
 	return 0;
 }
